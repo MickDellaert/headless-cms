02da89bf6b7e7b0c782923f8b81f55bed872d4c0
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2022-04-07 14:16:41 +0200

    complex-queries solutions

--- http-calls/exercises/2-complex-queries/6-total-price-complete.js
+++ http-calls/exercises/2-complex-queries/6-total-price-complete.js
@@ -1,25 +1,93 @@
 /**
- * Fill in the blanks to create a script 
+ * Fill in the blanks to create a script
  * that prints the total cost if someone would buy one of every item
  * taking into consideration that it's impossible to buy items that are out of stuck,
- * and taking the discount rates into account 
+ * and taking the discount rates into account
  */
 
- import "./qs.js";
- async function ex6() {
-   const query = qs.stringify(
-   {
-       _
-   }, 
-   {
-     encodeValuesOnly: true,
-   });
-   console.log("The query string", query);
- 
-   // call the matching endpoint and include the querystring after the ?
-   const baseUrl = _;
-   const response = await fetch(`${_}?${query}`);
-   const result = await response.json();
-   _
- }
- ex6();
+import "./qs.js";
+async function ex4() {
+  const query = qs.stringify(
+    {
+      fields: ["price", "name"],
+
+      populate: {
+        discount: {
+          fields: ["percentage"],
+        },
+      },
+      filters: {
+        outOfStock: {
+          $eq: "false",
+        },
+      },
+    },
+    {
+      encodeValuesOnly: true,
+    }
+  );
+  // console.log("The query string", query);
+
+  // call the matching endpoint and include the querystring after the ?
+  const baseUrl = "http://localhost:1337/api/products";
+  const response = await fetch(`${baseUrl}?${query}`);
+  const result = await response.json();
+
+  let totalPrice = 0;
+
+  for (let i = 0; i < result.data.length; i++) {
+    let price = result.data[i].attributes.price;
+
+    if (result.data[i].attributes.discount.data !== null) {
+      const discount =
+        result.data[i].attributes.discount.data.attributes.percentage;
+
+      const discountPerc = discount / 100;
+
+      price = Math.floor(price - price * discountPerc);
+    }
+
+    totalPrice += price;
+  }
+
+  console.log(totalPrice);
+}
+ex4();
+
+// other solution with else statement:
+
+// let totalPrice = 0;
+// let normalTotal = 0;
+// let discountTotal = 0;
+
+//  for (let i = 0; i < result.data.length; i++) {
+
+//     let price = result.data[i].attributes.price;
+//     // console.log(price);
+
+//     if (result.data[i].attributes.discount.data !== null) {
+
+//       const discount = result.data[i].attributes.discount.data.attributes.percentage;
+//       // console.log(discount);
+
+//       const discountPerc = discount / 100;
+//       // console.log(discountPerc);
+
+//       const discountPrice = price - price * discountPerc;
+//       // console.log(discountPrice);
+
+//       discountTotal += discountPrice;
+//     } else {
+
+//       const normalPrice = price;
+//       // console.log(price)
+//       normalTotal += normalPrice;
+//     }
+
+//     totalPrice = normalTotal + discountTotal;
+//   }
+
+//   console.log(totalPrice)
+
+// }
+// ex4();
--- http-calls/exercises/2-complex-queries/7-most-expensive.js
+++ http-calls/exercises/2-complex-queries/7-most-expensive.js
@@ -1,32 +1,65 @@
 /**
- * Fill in the blanks to create a script 
+ * Fill in the blanks to create a script
  * that prints the name of the most expensive item that someone can buy (after applying any discounts,
  * and not including any items that are out of stock)
  */
 
- import "./qs.js";
- async function searchProductByName(nameStr) {
-   const query = qs.stringify(
-   {
-       _
-   }, 
-   {
-     encodeValuesOnly: true,
-   });
-   console.log("The query string", query);
- 
-   // call the matching endpoint and include the querystring after the ?
-   const baseUrl = _;
-   const response = await fetch(`${_}?${query}`);
-   const result = await response.json();
-   _
- }
+import "./qs.js";
+async function mostExpensiveProduct() {
+  const query = qs.stringify(
+    {
+      fields: ["price", "name"],
 
- async function test() {
-  console.log("Products containing name", await searchProductByName("name"));
-  console.log("Products containing prog", await searchProductByName("prog"));
-  console.log("Products containing pro", await searchProductByName("pro"));
- }
+      populate: {
+        discount: {
+          fields: ["percentage"],
+        },
+      },
+      filters: {
+        outOfStock: {
+          $eq: "false",
+        },
+      },
+    },
+    {
+      encodeValuesOnly: true,
+    }
+  );
+  console.log("The query string", query);
 
- test();
+  // call the matching endpoint and include the querystring after the ?
+  const baseUrl = "http://localhost:1337/api/products";
+  const response = await fetch(`${baseUrl}?${query}`);
+  const result = await response.json();
 
+  let priceArr = [];
+
+  for (let i = 0; i < result.data.length; i++) {
+    let price = result.data[i].attributes.price;
+
+    if (result.data[i].attributes.discount.data !== null) {
+      const discount =
+        result.data[i].attributes.discount.data.attributes.percentage;
+
+      const discountPerc = discount / 100;
+
+      price = Math.floor(price - price * discountPerc);
+    }
+    priceArr.push(price);
+    // console.log(typeof price);
+    // console.log(priceArr);
+  }
+  const mostExpensive = Math.max(...priceArr);
+  const index = priceArr.indexOf(mostExpensive);
+
+  const mostExpensiveName = result.data[index].attributes.name;
+  console.log(mostExpensiveName);
+}
+
+// async function test() {
+//   console.log("Products containing name", await searchProductByName("name"));
+//   console.log("Products containing prog", await searchProductByName("prog"));
+//   console.log("Products containing pro", await searchProductByName("pro"));
+// }
+
+mostExpensiveProduct();
